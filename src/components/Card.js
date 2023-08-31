b8bd22ccc0c260f193f034c7e37da6fc234c9add
Minh <minh.seikel@gmail.com>
2023-08-31 16:56:16 -0700

    completion of Day3 - adding css styling, state and function to handle flipping card from front to back

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,30 +5,31 @@ import cover from "../assets/cover.jpg";
 import { useState } from "react";
 
 const Card = ({ index, card }) => {
-const [isFlipped, setFlipped] = useState(false);
+  const [isFlipped, setFlipped] = useState(false);
 
   const handleCardClick = () => {
-    setFlipped(!isFlipped);
+    if (!isFlipped) {
+      setFlipped(true);
+    }
   };
 
-
-
   return (
-    <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={handleCardClick}>
-      <div className="card-face front side">
-        <img src={cover} alt="front-face"/>
+    <div
+      className={`card ${isFlipped ? "flipped" : ""}`}
+      onClick={handleCardClick}
+    >
+      <div className="card-face front">
+        <img src={cover} alt="front-face" />
       </div>
-      <div className="card-face back side">
-        <img src={card.src} alt="back-face"/>
+      <div className="card-face back">
+        <img src={card.src} alt="back-face" />
       </div>
     </div>
-    
   );
 };
 
- Card.propTypes = {
+Card.propTypes = {
   card: PropTypes.object.isRequired,
-}; 
+};
 
 export default Card;
-
